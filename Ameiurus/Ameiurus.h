e0f349c79d2d4a8a8e02dd7fecf0aea15af6eed1
David Linhares <linhares.david@digipolitan.com>
2019-02-25 23:46:16 +0100

    :tada: Copy all from Ameiurus-melas project

---
+++ Ameiurus/Ameiurus.h
@@ -0,0 +1,19 @@
+//
+//  Ameiurus.h
+//  Ameiurus
+//
+//  Created by David Linhares on 25/02/2019.
+//  Copyright © 2019 David Linhares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Ameiurus.
+FOUNDATION_EXPORT double AmeiurusVersionNumber;
+
+//! Project version string for Ameiurus.
+FOUNDATION_EXPORT const unsigned char AmeiurusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ameiurus/PublicHeader.h>
+
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Ameiurus' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Ameiurus-melas
+  pod 'Alamofire', '~> 4.7'
+  pod 'SnapKit', '~> 4.0.0'
+  pod 'Toolbar'
+end
